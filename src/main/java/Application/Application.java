ef9ee54a85b6ad464464c4e7c560691530fb06a4
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-04-28 11:51:32 +0300

    Initial commit

---
+++ src/main/java/Application/Application.java
@@ -0,0 +1,15 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ivan on 12.4.2017 г..
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/Application/CreateRoom.java
@@ -0,0 +1,17 @@
+package Application;
+
+/**
+ * In Future we should use directly the Room Class :thinking:
+ * Created by ivan on 14.4.2017 г..
+ */
+public class CreateRoom {
+    private Long maxPlayers;
+
+    public Long getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(Long maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+}
