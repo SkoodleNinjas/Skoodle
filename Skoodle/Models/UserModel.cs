6ee3e5ee4fa6ac522281dd1051799210e0112086
Ivan Milev <ivanmilevTUES@gmail.com>
2017-10-13 12:19:34 +0300

    Adding /Main/Index Hello World page

---
+++ Skoodle/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Skoodle.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String UserName { get; set; }
+
+    }
+}
