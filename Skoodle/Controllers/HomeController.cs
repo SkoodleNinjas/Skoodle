756bad218f6a8d05fabf301556f2ff576fed7364
Nikolay Stanishev <nikolay@nikolay.nikolay.org>
2017-10-14 17:16:59 +0300

    Initial commit

---
+++ Skoodle/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skoodle.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult SPATestMethod()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult IndexPartial()
+        {
+            return PartialView("Index");
+        }
+    }
+}
