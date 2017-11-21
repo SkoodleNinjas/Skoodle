2831febc6451a1055ea18f02f04fd010ab6bcf97
Ivan Milev <ivanmilevTUES@gmail.com>
2017-11-21 21:53:19 +0200

    Adding CodeFirst migrations to the project

---
+++ Skoodle/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Skoodle.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Skoodle.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Skoodle.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Skoodle.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
