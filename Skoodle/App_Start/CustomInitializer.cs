19fd3c0dc9adb8ff6ce74a6df7d6294e82c9c97c
nikolaystanishev <stanishev.nikolay@gmail.com>
2018-01-10 23:10:19 +0200

    Change seeding of topic categories

--- Skoodle/App_Start/CustomInitializer.cs
+++ Skoodle/App_Start/CustomInitializer.cs
@@ -7,11 +7,15 @@ namespace Skoodle.App_Start
     {
         protected override void Seed(ApplicationDbContext context)
         {
-            context.Topics.Add(new Topic { Name = "Topic1", Category = "Category1" });
-            context.Topics.Add(new Topic { Name = "Topic2", Category = "Category1" });
-            context.Topics.Add(new Topic { Name = "Topic3", Category = "Category2" });
-            context.Topics.Add(new Topic { Name = "Topic4", Category = "Category2" });
-            context.Topics.Add(new Topic { Name = "Topic5", Category = "Category3" });
+            Cathegory Category1 = new Cathegory { CathegoryName = "Category1" };
+            Cathegory Category2 = new Cathegory { CathegoryName = "Category2" };
+            Cathegory Category3 = new Cathegory { CathegoryName = "Category3" };
+
+            context.Topics.Add(new Topic { Name = "Topic1", Category = Category1 });
+            context.Topics.Add(new Topic { Name = "Topic2", Category = Category1 });
+            context.Topics.Add(new Topic { Name = "Topic3", Category = Category2 });
+            context.Topics.Add(new Topic { Name = "Topic4", Category = Category2 });
+            context.Topics.Add(new Topic { Name = "Topic5", Category = Category3 });
 
             context.SaveChanges();
         }
